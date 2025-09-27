a628be9801414e05265a234a74f0f351490cf8b2
Hind Jammar <hindjoumar4@gmail.com>
2025-09-27 13:24:24 +0100

    feat added:new file pharmacyController

---
+++ backend/controllers/pharmacyController.js
@@ -0,0 +1,22 @@
+import Pharmacy from "../models/Pharmacy.js";
+
+export const getPharmacies = async (req, res) => {
+  const pharmacies = await Pharmacy.find({});
+  res.json(pharmacies);
+};
+
+export const createPharmacy = async (req, res) => {
+  const pharmacy = new Pharmacy(req.body);
+  const created = await pharmacy.save();
+  res.status(201).json(created);
+};
+
+export const updatePharmacy = async (req, res) => {
+  const pharmacy = await Pharmacy.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(pharmacy);
+};
+
+export const deletePharmacy = async (req, res) => {
+  await Pharmacy.findByIdAndDelete(req.params.id);
+  res.json({ message: "Pharmacy deleted" });
+};
